d4ef634dd859016084e991d82084c353f78d5c41
Matthew Tran <matt.h.tran@gmail.com>
2024-06-20 11:02:52 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// Write your code in this file!
-const currentUser = 'Grace Hopper';
+const currentUser = '';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
 
 const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
